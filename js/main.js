c8d9f030bda8241efe84aa0c694b54e42fd3bb83
Adam <adampugh@zoho.com>
2020-03-04 21:05:08 +0000

    update

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const purchasedImmortalHulk = []
 const immortalHulk = createComicList(31)
 
 const purchasedSilverSable = [1, 4, 5].map(el => `#${el}`)
-const silverSable = createComicList(35).filter(el => !purchasedSilverSable.includes(el))
+const silverSable = createComicList(35)
 
 const purchasedSpawn = []
 const spawn = createComicList(100)
@@ -16,6 +16,13 @@ const spiderMan2099Vol2 = createComicList(12)
 const purchasedS2099Vol3 = []
 const spiderMan2099Vol3 = createComicList(25)
 
+// update collected series
+$('.series-silver-sable .comic__grid__list__item').each((i, el) => purchasedSilverSable.includes($(el).text()))
+setTimeout(() => {
+    console.log($('.series-silver-sable .comic__grid__list__item'))
+}, 5000)    
+
+// .series-silver-sable
 
 // add comic panel
 const plus = $('#plus')
